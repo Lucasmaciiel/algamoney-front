5c75c03a488136d1164d4620bb5ea68b3db91986
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-02-16 16:19:38 -0300

    Formata data e valor utilizando pipes

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,20 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent {
 
   lancamentos = [
-    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: 'null', valor: 4.55, pessoa: 'Padaria do José' },
-      { tipo: 'DESPESA', descricao: 'Compra de Refrigerante', dataVencimento: '30/06/2017',
-      dataPagamento: '10/05/2020', valor: 4.55, pessoa: 'Mercado Jão' },
-      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: 'null', valor: 4.55, pessoa: 'Padaria Marcos' },
-      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: '10/06/2020', valor: 4.55, pessoa: 'Padaria do José' },
-      { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: '14/02/2021', valor: 4.55, pessoa: 'Açougue Galdino' },
-      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: 'null', valor: 4.55, pessoa: 'Padaria do José' },
-      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: '30/06/2017',
-      dataPagamento: 'null', valor: 4.55, pessoa: 'Super mercado Avenida' }
+    { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2017, 5, 30),
+      dataPagamento: new Date(), valor: 4.55, pessoa: 'Padaria do José' },
+      { tipo: 'DESPESA', descricao: 'Compra de Refrigerante', dataVencimento: new Date(2017, 6, 27),
+      dataPagamento: '10/05/2020', valor: 800000, pessoa: 'Mercado Jão' },
+      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: new Date(2018, 7, 13),
+      dataPagamento: new Date(), valor: 14312, pessoa: 'Padaria Marcos' },
+      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: new Date(2015, 4, 14),
+      dataPagamento: new Date(2020, 6, 10), valor: 800, pessoa: 'Padaria do José' },
+      { tipo: 'DESPESA', descricao: 'Compra de pão', dataVencimento: new Date(2019, 7, 20),
+      dataPagamento: new Date(2021, 2, 14), valor: 55000, pessoa: 'Açougue Galdino' },
+      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: new Date(2019, 1, 1),
+      dataPagamento: new Date(), valor: 3055, pessoa: 'Padaria do José' },
+      { tipo: 'ENTRADA', descricao: 'Compra de pão', dataVencimento: new Date(2020, 2, 3),
+      dataPagamento: new Date(), valor: 4555, pessoa: 'Super mercado Avenida' }
   ];
 
 }
