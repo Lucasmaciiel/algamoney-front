0c383e6ec0bb93599a0330e4db55ddb4759b338a
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-02-18 22:02:23 -0300

    Adiciona seletor de data (componente Calendar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import { TableModule } from 'primeng/table';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CalendarModule } from 'primeng/calendar';
+import { AppComponent } from './app.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { FormsModule } from '@angular/forms';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import {InputTextareaModule} from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
@@ -21,8 +22,8 @@ import {InputTextareaModule} from 'primeng/inputtextarea';
     NavbarComponent,
     PessoasPesquisaComponent,
     LancamentoCadastroComponent,
-    
-    
+
+
   ],
   imports: [
     BrowserModule,
@@ -32,7 +33,8 @@ import {InputTextareaModule} from 'primeng/inputtextarea';
     TableModule,
     TooltipModule,
     FormsModule,
-    InputTextareaModule
+    InputTextareaModule,
+    CalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
