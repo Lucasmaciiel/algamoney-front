6715852a365510725f040754407b50429c059aec
Lucasmaciiel <lucasmacielgois@gmail.com>
2021-02-23 21:57:15 -0300

    Cria componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-ca
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { MessageComponent } from './message/message.component';
+import { LancamentosGridComponent } from './lancamentos-grid/lancamentos-grid.component';
+import { PessoasGridComponent } from './pessoas-grid/pessoas-grid.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +31,9 @@ import { MessageComponent } from './message/message.component';
     PessoasPesquisaComponent,
     LancamentoCadastroComponent,
     PessoaCadastroComponent,
-    MessageComponent
+    MessageComponent,
+    LancamentosGridComponent,
+    PessoasGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pessoas-grid/pessoas-grid.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoas-grid',
+  templateUrl: './pessoas-grid.component.html',
+  styleUrls: ['./pessoas-grid.component.css']
+})
+export class PessoasGridComponent {
+
+  @Input() pessoas = [];
+
+
+}
